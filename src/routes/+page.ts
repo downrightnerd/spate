1e12ffcd3262d1d34211969e16e951e08b5d42b3
downrightnerd <decoykorea@gmail.com>
2023-12-31 06:24:50 +0900

    Update +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { getPosts } from '$lib/utils/sanity';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-
+export const prerender = true;
 export const load = (async () => {
 	const posts = await getPosts();
 
